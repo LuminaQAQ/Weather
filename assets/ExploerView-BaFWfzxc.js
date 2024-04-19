69a6dc0079dab6bbe100aa87f1f0129e2eee8fa6
LuminaQAQ <1710884533@qq.com>
2024-04-19 16:06:39 +0800

    Updates

---
+++ assets/ExploerView-BaFWfzxc.js
@@ -0,0 +1,1 @@
+import{_ as s,a,b as o,c,e as r,t as n,i}from"./index-YRYOrCpT.js";const p={setup(){const e=a("explore");return i("$axios"),{title:e}}},l={class:"main-explore-wrap"};function _(e,d,f,t,x,u){return o(),c("section",l,[r("h1",null,n(t.title),1)])}const $=s(p,[["render",_],["__scopeId","data-v-c85319cd"]]);export{$ as default};
